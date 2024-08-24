76f02a2a08e317d5ce44e007d01e3eae5114d349
brocolis é bom <134985029+n1lima@users.noreply.github.com>
2024-08-24 16:01:55 -0300

    colocando mais um exercício

---
+++ vetores/exercicio2.js
@@ -0,0 +1,20 @@
+/* 7.2 Faça um programa que leia um vetor de números inteiros e exiba o maior elemento presente no vetor */
+var readline = require('readline-sync')  
+
+const quantity = Number(parseInt(readline.question('Digite a quantidade de elementos: ')))
+
+let array = []
+
+for(let i = 0; i<quantity; i++){
+    let number = Number(readline.questionInt(`Digite o valor do elemento ${i + 1}: `))
+    array.push(number)
+}
+
+let maior = 0
+for(let i = 0; i<array.length; i++){
+    if (maior<array[i]){
+        maior = array[i]
+    }
+}
+
+console.log(`O maior numero desse array eh: ${maior}`)
